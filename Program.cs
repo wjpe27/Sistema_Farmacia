b636866c69d6dec53646de3d2f19732210ed6f8d
Wilher J. Polanco Eustate <86754956+wjpe27@users.noreply.github.com>
2025-04-22 20:27:53 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SistemaGestionFarmacias.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar el DbContext con la cadena de conexión
+builder.Services.AddDbContext<SistemaFarmaciasContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Para que al navegar a “/” sirva por defecto login.html
+var defaultFilesOptions = new DefaultFilesOptions();
+defaultFilesOptions.DefaultFileNames.Clear();
+defaultFilesOptions.DefaultFileNames.Add("login.html");
+app.UseDefaultFiles(defaultFilesOptions);
+
+// Servir archivos estáticos (wwwroot)
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
